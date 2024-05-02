b0fb666fcf778790e715f07f0bd04e7bbb3a04d9
alfiechalfoun <alfie.chalfoun@gmail.com>
2024-05-02 20:40:16 +0100

    loding the FER daterset

--- emotion ai.py
+++ emotion ai.py
@@ -1,31 +1,41 @@
-import os
-import cv2
+# imports
+import cv2 
+import tensorflow.keras as tf
+import matplotlib.pyplot as plt 
+from tensorflow.keras import datasets, layers, models 
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import pandas as pd 
-import time
 
-FER = pd.read_csv('/Users/alfie/Documents/school /computer science /courswork /FER-2013 data set ')
+emotions = ['angry','disgust','fear','happy','neutral','sad','surprise']
+FER_path = '/Users/alfie/Documents/school /computer science /courswork /FER-2013 data set /fer2013.csv'
 
-(x_train, y_train), (x_test, y_test) = FER.load_data()
+#loading the FER_2013 daterset 
+faces = pd.read_csv(FER_path)
+faces['pixels'] = faces['pixels'].apply(lambda x: np.fromstring(x, dtype=int, sep=' ').reshape(48, 48) / 255.0)
 
+# splitting the data into traning and testing 
+traning_data = faces[faces['Usage'] == 'Training'] 
+test_data =  faces[faces['Usage'] == 'PublicTest']
 
-x_train = tf.keras.utils.normalize(x_train, axis=1 )
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+training_images = np.stack(traning_data['pixels'].values)
+training_labels = traning_data['emotion'].values
+testing_images = np.stack(test_data['pixels'].values)
+testing_labels = test_data['emotion'].values
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
-
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-model.fit(x_train,y_train,epochs=10 )
-
-# model.save('FER_model.h5')
-
-# model = tf.keras.models.load_model('FER_model.h5')
+training_images = training_images.reshape(-1, 48, 48, 1)
+testing_images = testing_images.reshape(-1, 48, 48, 1)
 
 
+
+
+
+# showing the image 
+plt.figure(figsize=(10,10))
+for i in range (16):
+    plt.subplot(4,4,i+1)
+    plt.xticks ([])
+    plt.yticks ([])
+    plt.imshow(testing_images[i,:,:,0], cmap=plt.cm.gray)
+    emotions_lable = emotions[testing_labels[i]]
+    plt.xlabel(emotions_lable)
+plt.show()
--- test.py
+++ test.py
@@ -45,3 +45,37 @@ while os.path.isfile(f'/Users/alfie/Documents/school /computer science /courswor
         print(f'error is {e}')
     finally:
         image_number += 1
+
+
+
+
+
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pandas as pd 
+import time
+
+FER = pd.read_csv('/Users/alfie/Documents/school /computer science /courswork /FER-2013 data set ')
+
+(x_train, y_train), (x_test, y_test) = FER.load_data()
+
+
+x_train = tf.keras.utils.normalize(x_train, axis=1 )
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train,y_train,epochs=10 )
+
+# model.save('FER_model.h5')
+
+# model = tf.keras.models.load_model('FER_model.h5')
