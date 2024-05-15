9557cc63898964013fd9d1517035557e93fa784c
alfiechalfoun <alfie.chalfoun@gmail.com>
2024-05-15 11:25:50 +0100

    creating classes

--- emotion_detection_model/emotion_ai.py
+++ emotion_detection_model/emotion_ai.py
@@ -4,128 +4,150 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.models import Sequential 
-from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D,BatchNormalization 
-from tensorflow.keras.metrics import categorical_accuracy 
-from tensorflow.keras.callbacks import ModelCheckpoint 
-from tensorflow.keras.callbacks import EarlyStopping 
-from tensorflow.keras.optimizers import * 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator 
- 
-tf.config.threading.set_intra_op_parallelism_threads(5)
-tf.config.threading.set_inter_op_parallelism_threads(3)
+from tensorflow.keras.models import Sequential  # type: ignore
+from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D,BatchNormalization  # type: ignore
+from tensorflow.keras.metrics import categorical_accuracy  # type: ignore
+from tensorflow.keras.callbacks import ModelCheckpoint  # type: ignore
+from tensorflow.keras.callbacks import EarlyStopping  # type: ignore
+from tensorflow.keras.optimizers import *  # type: ignore
+from tensorflow.keras.preprocessing.image import ImageDataGenerator  # type: ignore
+from tensorflow.keras.models import load_model  # type: ignore
 
-if tf.config.experimental.list_physical_devices('GPU'):
-    physical_devices = tf.config.list_physical_devices('GPU')
-    try:
-        tf.config.experimental.set_memory_growth(physical_devices[0], True)
-    except:
-        pass
+def limit_power():
+    tf.config.threading.set_intra_op_parallelism_threads(5)
+    tf.config.threading.set_inter_op_parallelism_threads(3)
 
-def loading_data():
-    # lableing
-    emotions = ['angry','disgust','fear','happy','neutral','sad','surprise']
-    FER_path = '/Users/alfie/Documents/school /computer science /courswork /FER-2013 data set /fer2013.csv'
+    if tf.config.experimental.list_physical_devices('GPU'):
+        physical_devices = tf.config.list_physical_devices('GPU')
+        try:
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        except:
+            pass
 
-    #loading the FER_2013 daterset 
-    faces = pd.read_csv(FER_path)
-    faces['pixels'] = faces['pixels'].apply(lambda x: np.fromstring(x, dtype=int, sep=' ').reshape(48, 48) / 255.0)
 
-    # splitting the data into traning and testing 
-    traning_data = faces[faces['Usage'] == 'Training'] 
-    test_data =  faces[faces['Usage'] == 'PublicTest']
+class FER13Data():
+    def __init__(self):
+        # lableing
+        self.emotions = ['angry','disgust','fear','happy','neutral','sad','surprise']
+        self.FER_path = '/Users/alfie/Documents/school /computer science /courswork /FER-2013 data set /fer2013.csv'
 
-    # prpering the data
-    training_images = np.stack(traning_data['pixels'].values)
-    training_labels = traning_data['emotion'].values
-    testing_images = np.stack(test_data['pixels'].values)
-    testing_labels = test_data['emotion'].values
-    training_images = training_images.reshape(-1, 48, 48, 1)
-    testing_images = testing_images.reshape(-1, 48, 48, 1)
+        #loading the FER_2013 daterset 
+        self.faces = pd.read_csv(self.FER_path)
+        self.faces['pixels'] = self.faces['pixels'].apply(lambda x: np.fromstring(x, dtype=int, sep=' ').reshape(48, 48) / 255.0)
+
+        # splitting the data into traning and testing 
+        self.traning_data = self.faces[self.faces['Usage'] == 'Training'] 
+        self.test_data =  self.faces[self.faces['Usage'] == 'PublicTest']
+
+    def prepering_data(self):
+        self.training_images = np.stack(self.traning_data['pixels'].values)
+        self.training_labels = self.traning_data['emotion'].values
+        self.testing_images = np.stack(self.test_data['pixels'].values)
+        self.testing_labels = self.test_data['emotion'].values
+        self.training_images = self.training_images.reshape(-1, 48, 48, 1)
+        self.testing_images = self.testing_images.reshape(-1, 48, 48, 1)
+        
+
+
+class Modle(FER13Data):
+    def __init__(self):
+        super().__init__()
+        
+
+    # crating my model
+    def create_modle(self): 
+        super().prepering_data()
     
-    return(training_images, training_labels, testing_images, testing_labels)
+        self.model = Sequential()
+        self.shape = (48,48,1)
+        self.model.add(Conv2D(32, (3,3), activation='relu', input_shape = self.shape, padding = 'same')) 
+        self.model.add(Conv2D(32,(3,3), activation='relu', padding = "same"))
+        self.model.add(Conv2D(32,(3,3),activation='relu', padding = 'same'))
+        self.model.add(BatchNormalization())
+        self.model.add(MaxPooling2D((2,2)))
+        self.model.add(Dropout(0.25))
 
+        self.model.add(Conv2D(64,(3,3), activation='relu', padding = 'same'))
+        self.model.add(Conv2D(64,(3,3), activation = 'relu',padding = 'same'))
+        self.model.add(Conv2D(64,(3,3), activation ='relu', padding = 'same'))
+        self.model.add(BatchNormalization())
+        self.model.add(MaxPooling2D((2,2)))
+        self.model.add(Dropout(0.25))
 
-# crating my model
-def create_modle(): 
-    model = Sequential()
-    shape = (48,48,1)
-    model.add(Conv2D(32, (3,3), activation='relu', input_shape = shape, padding = 'same'))
-    model.add(Conv2D(32,(3,3), activation='relu', padding = "same"))
-    model.add(Conv2D(32,(3,3),activation='relu', padding = 'same'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D((2,2)))
-    model.add(Dropout(0.25))
+        self.model.add(Conv2D(128, (3,3), activation ='relu'))
+        self.model.add(Conv2D(128, (3,3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(128,(3,3), activation = 'relu', padding = 'same'))
+        self.model.add(BatchNormalization())
+        self.model.add(MaxPooling2D((2,2)))
+        self.model.add(Dropout(0.25))
 
-    model.add(Conv2D(64,(3,3), activation='relu', padding = 'same'))
-    model.add(Conv2D(64,(3,3), activation = 'relu',padding = 'same'))
-    model.add(Conv2D(64,(3,3), activation ='relu', padding = 'same'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D((2,2)))
-    model.add(Dropout(0.25))
+        self.model.add(Flatten())
+        self.model.add(Dense(128, activation='relu'))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(7))
+        self.model.add(Activation('softmax'))
+        self.model.summary()
 
-    model.add(Conv2D(128, (3,3), activation ='relu'))
-    model.add(Conv2D(128, (3,3), activation = 'relu', padding = 'same'))
-    model.add(Conv2D(128,(3,3), activation = 'relu', padding = 'same'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D((2,2)))
-    model.add(Dropout(0.25))
+    def get_modle_layout(self):
+        self.create_modle()
+        self.model.summary()
 
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(7))
-    model.add(Activation('softmax'))
-    return(model)
+    def train_modle(self):
+        self.create_modle()
+        limit_power()
 
-def train_modle(model):
-    training_images, training_labels, testing_images, testing_labels = loading_data()
-# making the modle stop traning if no improvment are found aveter 3 epsolons 
-    early_stop = EarlyStopping(
-        monitor='val_loss', 
-        min_delta=0.001, 
-        patience=3, 
-        verbose=1, 
-        restore_best_weights=True)
+    # making the modle stop traning if no improvment are found aveter 3 epsolons 
+        self.early_stop = EarlyStopping(
+            monitor='val_loss', 
+            min_delta=0.001, 
+            patience=3, 
+            verbose=1, 
+            restore_best_weights=True)
 
-    # randomising the imiages 
-    train_datagen = ImageDataGenerator(  
-        rotation_range=20,  
-        width_shift_range=0.2,  
-        height_shift_range=0.2,  
-        shear_range=0.2,  
-        zoom_range=0.2,  
-        horizontal_flip=True,  
-        fill_mode='nearest'  
-    )
+        # randomising the imiages 
+        self.train_datagen = ImageDataGenerator(  
+            rotation_range=20,  
+            width_shift_range=0.2,  
+            height_shift_range=0.2,  
+            shear_range=0.2,  
+            zoom_range=0.2,  
+            horizontal_flip=True,  
+            fill_mode='nearest'  
+        )
 
-    train_generator = train_datagen.flow(
-        training_images,
-        training_labels,
-        batch_size=32,
-        shuffle=True
-    )
-    # traning the modle 
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        self.train_generator = self.train_datagen.flow(
+            self.training_images,
+            self.training_labels,
+            batch_size=32,
+            shuffle=True
+        )
+        # traning the modle 
+        self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(
-        training_images, 
-        training_labels, 
-        epochs=20, 
-        validation_data=(testing_images, testing_labels),
-        callbacks = [early_stop],
-        shuffle = True)
+        self.model.fit(
+            self.training_images, 
+            self.training_labels, 
+            epochs=20, 
+            validation_data=(self.testing_images, self.testing_labels),
+            callbacks = [self.early_stop],
+            shuffle = True)
+        
+        self.model.save('emotion_detection_modle.h5')
+
+    def load_modle(self):
+        self.modle = load_model('emotion_detection_model/emotion_detection_modle.h5')
 
     # testing the modle 
-    test_loss, test_accuracy = model.evaluate(testing_images, testing_labels)
-    print(f'the loss is {test_loss} \n the acuracy is {test_accuracy}')
-
-# saving modle 
+    def get_acuracy(self):
+        self.load_modle()
+        super().prepering_data()
+        test_loss, test_accuracy = self.modle.evaluate(self.testing_images, self.testing_labels)
+        print(f'the loss is {test_loss} \n the acuracy is {test_accuracy}')
 
 
 if __name__ == '__main__':
-    modle = create_modle()
-    train_modle(modle)
-    model.save('emotion_detection_modle.h5')
+    modle = Modle()
+    modle.get_modle_layout()
+    
--- emotion_detection_model/testing modle.py
+++ emotion_detection_model/testing modle.py
@@ -1,13 +1,9 @@
 import tensorflow as tf 
-import matplotlib as plt 
+import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
-from emotion_ai import loading_data
-import pandas as pd 
-model_path = ('emotion_detection_model/emotion_detection_modle.h5')
+from emotion_ai import Modle, FER13Data
 
-model = load_model(model_path)
+# data = FER13Data()
 
-training_images, training_labels, testing_images, testing_labels = loading_data()
-
-test_loss, test_accuracy = model.evaluate(testing_images, testing_labels)
-print(f'the loss is {test_loss} \n the acuracy is {test_accuracy}')
+modle = Modle()
+modle.get_modle_layout()
